612e857dee2db956ab3b4459c7bdfab8700296eb
Chingkheinganba Irungbam <99580046+Chingkheinganba06@users.noreply.github.com>
2022-02-21 00:56:45 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '918414802914', 'Draken-kun, m)
+  this.sendContact(m.chat, '918414802914', 'Draken-kun', m)
 }
 handler.help = ['owner/creator']
 handler.tags = ['info']
